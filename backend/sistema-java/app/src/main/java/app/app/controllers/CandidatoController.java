b444bef2119be5a8f8c768e701879995c9b65ae1
pietroCAFFETTANI <p.caffettani@gmail.com>
2026-05-20 01:09:49 -0300

    Terraform 99% finalizado + Front End atualizado

--- backend/sistema-java/app/src/main/java/app/app/controllers/CandidatoController.java
+++ backend/sistema-java/app/src/main/java/app/app/controllers/CandidatoController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/api")
 public class CandidatoController {

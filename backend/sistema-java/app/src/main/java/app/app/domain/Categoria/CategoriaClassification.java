24d709a142cdaea4f4b7a21b3eade2421192fe6a
Gabriel Faravola <gabrielfaravola@gmail.com>
2026-08-05 23:44:06 -0300

    Adição gráfico radar

--- backend/sistema-java/app/src/main/java/app/app/domain/Categoria/CategoriaClassification.java
+++ backend/sistema-java/app/src/main/java/app/app/domain/Categoria/CategoriaClassification.java
@@ -1,9 +1,14 @@
 package app.app.domain.Categoria;
 
 import app.app.domain.Candidato.Candidato;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name="categoria_classification")
 public class CategoriaClassification {
 
@@ -13,6 +18,7 @@ public class CategoriaClassification {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "candidato_id")
+    @JsonIgnore
     private Candidato candidato;
 
     @Enumerated(EnumType.STRING)
